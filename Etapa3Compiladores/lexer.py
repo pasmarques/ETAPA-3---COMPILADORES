49439d401362e8e07309a9175e41a5e0c4053273
Pedro Affonso Silva Marques <89036165+pasmarques@users.noreply.github.com>
2024-05-06 19:49:51 -0300

    mudando alguns pontos para melhorar legibilidade

--- Etapa3Compiladores/lexer.py
+++ Etapa3Compiladores/lexer.py
@@ -10,23 +10,29 @@ import sys
 # Visando a manipulação de cada token e erro para impressão das tabelas e relatórios de erro criei :
 
 # Classe que simula um token da linguagem cic 2024.1
+
+
 class Token:
     def __init__(self, tipo, valor, linha, coluna):
-        self.tipo = tipo      # Tipo do token (identificador, palavra reservada, operador, etc.)
+        # Tipo do token (identificador, palavra reservada, operador, etc.)
+        self.tipo = tipo
         self.valor = valor    # Valor do token (se aplicável)
         self.linha = linha    # Número da linha onde o token foi encontrado
         self.coluna = coluna  # Número da coluna onde o token foi encontrado
-        
+
 # Classe que simula um erro da linguagem cic 2024.1
+
+
 class Erro:
     def __init__(self, msg, linha, coluna):
-        self.msg = msg # Mensagem de erro
+        self.msg = msg  # Mensagem de erro
         self.linha = linha        # Número da linha onde o erro foi encontrado
         self.coluna = coluna      # Número da coluna onde o erro foi encontrado
-        
+
     def __str__(self):
         return f'Erro linha {self.linha} coluna {self.coluna}: {self.msg}'
-    
+
+
 def criar_token(estado, lexema, linha, coluna):
     # Dicionário que mapeia estados para tipos de tokens
     tipos_tokens = {
@@ -45,7 +51,7 @@ def criar_token(estado, lexema, linha, coluna):
         'q39': 'TK_COMENTARIO',
         'q42': 'TK_ATRIBUICAO',
         'q43': 'TK_DIFERENTE',
-        'q45': 'TK_IGUAL', 
+        'q45': 'TK_IGUAL',
         'q49': 'TK_MAIORIGUAL',
         'q50': 'TK_&',
         'q51': 'TK_RESERVADAS',
@@ -58,7 +64,8 @@ def criar_token(estado, lexema, linha, coluna):
     }
 
     # Obtém o tipo de token do estado atual
-    tokenDoEstado = tipos_tokens.get(estado, 'Desconhecido') # Se o estado não for reconhecido, assume 'Desconhecido'
+    # Se o estado não for reconhecido, assume 'Desconhecido'
+    tokenDoEstado = tipos_tokens.get(estado, 'Desconhecido')
 
     # Verifica se o token é um identificador reservado
     if tokenDoEstado == "TK_RESERVADAS":
@@ -68,12 +75,15 @@ def criar_token(estado, lexema, linha, coluna):
     return Token(tokenDoEstado, lexema, linha, coluna)
 
 # Cria objeto erro
+
+
 def criar_erro(lexema, linha, coluna):
     return Erro(lexema, linha, coluna)
 
+
 def defineMsgErro(estado):
     # Dicionário que mapeia estados de erro para mensagens de erro correspondentes
-    erroEstados ={
+    erroEstados = {
         'ERRO_INICIAL': 'caracter nao inicia token',
         'ERRO_CADEIA': 'cadeia nao fechada',
         'ERRO_DATA': 'data mal formatada',
@@ -83,17 +93,18 @@ def defineMsgErro(estado):
         'ERRO_IDKW': 'variavel ou id mal formatado',
         'ERRO_ENDERECO': 'endereco mal formatado'
     }
-    
+
     # Obtém a mensagem de erro correspondente ao estado
     msg = erroEstados.get(estado, 'Caracter nao inicia token')
 
     # Retorna a mensagem de erro
     return msg
 
+
 def verifica_se_e_reservado(lexema):
     # Imprime uma mensagem para verificar o lexema sendo verificado
     print(f'Verificando lexema: {lexema}')
-    
+
     # Dicionário que mapeia palavras reservadas para seus tokens correspondentes
     palavras_reservadas = {
         'rotina': 'TK_ROTINA',
@@ -105,23 +116,27 @@ def verifica_se_e_reservado(lexema):
         'para': 'TK_PARA',
         'enquanto': 'TK_ENQUANTO'
     }
-    
+
     # Retorna o token correspondente ao lexema, ou 'TK_KW_INVALIDA' se não for uma palavra reservada
     return palavras_reservadas.get(lexema, 'TK_KW_INVALIDA')
 
 
 def abrir_arquivo(nome_arquivo):
     try:
-        arquivo = open(nome_arquivo, 'r')  # Tenta abrir o arquivo em modo de leitura
+        # Tenta abrir o arquivo em modo de leitura
+        arquivo = open(nome_arquivo, 'r')
         linhas = arquivo.readlines()  # Lê todas as linhas do arquivo e armazena em uma lista
         arquivo.close()  # Fecha o arquivo após a leitura
         return linhas  # Retorna as linhas lidas do arquivo
     except FileNotFoundError:  # Captura a exceção se o arquivo não for encontrado
         print(f"Erro: Arquivo '{nome_arquivo}' não encontrado.")
-        sys.exit(1)  # Sai do programa com código de erro 1 em caso de arquivo não encontrado
+        # Sai do programa com código de erro 1 em caso de arquivo não encontrado
+        sys.exit(1)
     except Exception as e:  # Captura qualquer outra exceção não prevista
-        print(f"Erro ao abrir o arquivo: {e}")  # Imprime uma mensagem de erro genérica
-        sys.exit(1)  # Sai do programa com código de erro 1 em caso de erro não previsto
+        # Imprime uma mensagem de erro genérica
+        print(f"Erro ao abrir o arquivo: {e}")
+        # Sai do programa com código de erro 1 em caso de erro não previsto
+        sys.exit(1)
 
 
 def proximo_token_e_erros(linhasArq, lista_tokens, lista_erros):
@@ -129,8 +144,8 @@ def proximo_token_e_erros(linhasArq, lista_tokens, lista_erros):
 
     # Lista de estados de aceitação
     estados_aceitacao = ['q2', 'q7', 'q16', 'q21', 'q24', 'q30', 'q32', 'q33',
-                         'q34', 'q35', 'q36', 'q37', 'q39', 'q42', 'q43', 'q45', 
-                         'q49', 'q50', 'q51', 'q52', 'q53', 'q54', 'q55', 'q56', 
+                         'q34', 'q35', 'q36', 'q37', 'q39', 'q42', 'q43', 'q45',
+                         'q49', 'q50', 'q51', 'q52', 'q53', 'q54', 'q55', 'q56',
                          'q57']
 
     # Lista de estados que não são dígitos
@@ -140,11 +155,11 @@ def proximo_token_e_erros(linhasArq, lista_tokens, lista_erros):
     l = 1
 
     # Indica se estamos ignorando o conteúdo entre <<< e >>>
-    ignore_mode = False  
+    ignore_mode = False
     # Linha onde o modo de ignorar iniciou
-    ignore_start_line = 0  
+    ignore_start_line = 0
     # Coluna onde o modo de ignorar iniciou
-    ignore_start_col = 0  
+    ignore_start_col = 0
 
     # Loop sobre as linhas do arquivo
     for linha in linhasArq:
@@ -161,7 +176,7 @@ def proximo_token_e_erros(linhasArq, lista_tokens, lista_erros):
                 # Se encontrou o fechamento do modo de ignorar
                 if caracter == '>' and linha[c+1] == '>' and linha[c+2] == '>':
                     ignore_mode = False
-                    c += 2  # Avança para o último '>' de '>>>'
+                    c += 3  # Avança para o último '>' de '>>>'
                     continue
                 else:
                     c += 1
@@ -175,7 +190,7 @@ def proximo_token_e_erros(linhasArq, lista_tokens, lista_erros):
             if caracter == '<' and linha[c:c+2] == '<<':
                 ignore_mode = True
                 ignore_start_line = l
-                ignore_start_col = c - 1  # Contabiliza o primeiro '<'
+                ignore_start_col = c  # Contabiliza o primeiro '<'
                 c += 2  # Avança para o próximo caracter após '<<'
                 continue
 
@@ -185,12 +200,13 @@ def proximo_token_e_erros(linhasArq, lista_tokens, lista_erros):
                     lexema = ''
                     estado = 'q0'
                     continue
-                elif estado in estados_naoDigito: 
+                elif estado in estados_naoDigito:
                     if estado == 'q51':
                         lexema = lexema[:-1]
                         lexemaReserved = verifica_se_e_reservado(lexema)
                         if lexemaReserved != "TK_KW_INVALIDA":
-                            lista_tokens.append(criar_token(estado, lexema, l, c - len(lexema)))
+                            lista_tokens.append(criar_token(
+                                estado, lexema, l, c - len(lexema)))
                             lexema = ''
                             estado = 'q0'
                             c -= 1
@@ -201,16 +217,18 @@ def proximo_token_e_erros(linhasArq, lista_tokens, lista_erros):
                             lista_erros.append(erro)
                             lexema = ''
                             estado = 'q0'
-                            c-=1
+                            c -= 1
                             continue
                     lexema = lexema[:-1]  # Remove o último caractere
-                    lista_tokens.append(criar_token(estado, lexema, l, c - len(lexema)))
+                    lista_tokens.append(criar_token(
+                        estado, lexema, l, c - len(lexema)))
                     lexema = ''
                     estado = 'q0'
                     c -= 1
                     continue
 
-                lista_tokens.append(criar_token(estado, lexema, l, (c+1)-len(lexema)))
+                lista_tokens.append(criar_token(
+                    estado, lexema, l, (c+1)-len(lexema)))
                 lexema = ''
                 estado = 'q0'
                 continue
@@ -279,7 +297,7 @@ def proximo_token_e_erros(linhasArq, lista_tokens, lista_erros):
                 msg = defineMsgErro(estado)
                 erro = criar_erro(msg, l, c)
                 lista_erros.append(erro)
-                # Não retorna token se comentário for aberto e não fechado
+                # Não retorna token se comentário for aberto com mal formatação
                 return None
 
         l += 1
@@ -287,15 +305,17 @@ def proximo_token_e_erros(linhasArq, lista_tokens, lista_erros):
     # Se o modo de ignorar ainda estiver ativado no final do arquivo
     if ignore_mode:
         msg = "Comentario aberto (<<<) sem fechamento (>>>)"
-        erro = criar_erro(msg, ignore_start_line, ignore_start_col+1)
+        erro = criar_erro(msg, ignore_start_line, ignore_start_col)
         lista_erros.append(erro)
 
-    return None # Se nenhum token for encontrado
+    return None  # Se nenhum token for encontrado
 
 # Função que simula a transição do afd
 # Os estados de erros não estão presentes no autômato, mas eles foram postos aqui para conseguir depurar cada um
-def transicaoAFD(estado,caracter):
-    
+
+
+def transicaoAFD(estado, caracter):
+
     if estado == 'q0':
         if caracter.isdigit():
             estado = 'q1'
@@ -304,7 +324,7 @@ def transicaoAFD(estado,caracter):
         elif caracter == '.':
             estado = 'q8'
         elif caracter == '"':
-            estado = 'q22'   
+            estado = 'q22'
         elif 'a' <= caracter <= 'z':
             estado = 'q28'
         elif caracter == '|':
@@ -335,7 +355,7 @@ def transicaoAFD(estado,caracter):
             estado = 'q44'
         elif caracter == '#':
             estado = 'q38'
-        #elif caracter.isspace():
+        # elif caracter.isspace():
          #   estado = 'q0'
         else:
             estado = 'ERRO_INICIAL'
@@ -347,7 +367,7 @@ def transicaoAFD(estado,caracter):
         elif caracter.isdigit():
             estado = 'q3'
         else:
-            estado = 'q2' 
+            estado = 'q2'
     elif estado == 'q3':
         if caracter == '/':
             estado = 'q27'
@@ -370,7 +390,7 @@ def transicaoAFD(estado,caracter):
         if caracter.isdigit():
             estado = 'q12'
         else:
-            estado = 'q2'  
+            estado = 'q2'
     elif estado == 'q8':
         if caracter.isdigit():
             estado = 'q5'
@@ -382,12 +402,12 @@ def transicaoAFD(estado,caracter):
         elif caracter == 'e':
             estado = 'q9'
         else:
-            estado = 'q7' 
+            estado = 'q7'
     elif estado == 'q6':
         if caracter.isdigit():
             estado = 'q6'
         else:
-            estado = 'q7' 
+            estado = 'q7'
     elif estado == 'q9':
         if caracter.isdigit():
             estado = 'q11'
@@ -411,14 +431,14 @@ def transicaoAFD(estado,caracter):
         else:
             estado = 'ERRO_ENDERECO'
     elif estado == 'q19':
-        if caracter.isdigit() or  'A' <= caracter <= 'F':
+        if caracter.isdigit() or 'A' <= caracter <= 'F':
             estado = 'q20'
         else:
             estado = 'ERRO_ENDERECO'
     elif estado == 'q20':
-         if (caracter.isdigit()) or  ('A' <= caracter <= 'F'):
+        if (caracter.isdigit()) or ('A' <= caracter <= 'F'):
             estado = 'q20'
-         else:
+        else:
             estado = 'q21'
     elif estado == 'q27':
         if caracter.isdigit():
@@ -429,7 +449,7 @@ def transicaoAFD(estado,caracter):
         if caracter.isdigit():
             estado = 'q61'
         else:
-            estado ='ERRO_DATA'
+            estado = 'ERRO_DATA'
     elif estado == 'q61':
         if caracter == '/':
             estado = 'q62'
@@ -478,10 +498,10 @@ def transicaoAFD(estado,caracter):
     elif estado == 'q22':
         if caracter != '\n' and caracter == '"':
             estado = 'q24'
-        elif caracter == '\n' :
+        elif caracter == '\n':
             estado = 'ERRO_CADEIA'
         else:
-            estado='q22'
+            estado = 'q22'
     elif estado == 'q28':
         if 'A' <= caracter <= 'Z':
             estado = 'q29'
@@ -492,13 +512,13 @@ def transicaoAFD(estado,caracter):
     elif estado == 'q29':
         if 'a' <= caracter <= 'z':
             estado = 'q31'
-        elif not('a' <= caracter <= 'z') and not ('A' <= caracter <= 'Z'):
+        elif not ('a' <= caracter <= 'z') and not ('A' <= caracter <= 'Z'):
             estado = 'q30'
         else:
             estado = 'ERRO_ID_E_KW'
     elif estado == 'q31':
         if 'A' <= caracter <= 'Z':
-            estado = 'q29' 
+            estado = 'q29'
         elif not ('a' <= caracter <= 'z') and not ('A' <= caracter <= 'Z'):
             estado = 'q30'
         else:
@@ -561,39 +581,43 @@ def printaTokens(listaTokens):
     print("+-----+-----+---------------+---------------------------+")
     print("| LIN | COL |    TOKEN      |           LEXEMA          |")
     print("+-----+-----+---------------+---------------------------+")
-   
+
     # Inicializa a variável para acompanhar a linha anterior
     linha_anterior = None
-    
+
     # Lista de tipos especiais que precisam ser formatados de forma diferente
-    tipos_especiais = ['TK_INT', 'TK_FLOAT', 'TK_DATA', 'TK_CADEIA','TK_ENDERECO', 'TK_ID', 'TK_ROTINA', 'TK_FIM_ROTINA', 'TK_SE', 'TK_SENAO', 'TK_IMPRIMA', 'TK_LEIA', 'TK_PARA', 'TK_ENQUANTO']
-    
+    tipos_especiais = ['TK_INT', 'TK_FLOAT', 'TK_DATA', 'TK_CADEIA', 'TK_ENDERECO', 'TK_ID', 'TK_ROTINA',
+                       'TK_FIM_ROTINA', 'TK_SE', 'TK_SENAO', 'TK_IMPRIMA', 'TK_LEIA', 'TK_PARA', 'TK_ENQUANTO']
+
     # Itera sobre os tokens da lista
     for token in listaTokens:
         # Verifica se o token está na mesma linha que o anterior
         if token.linha == linha_anterior:
             linha_str = " "*5  # Se sim, não precisa imprimir o número da linha novamente
         else:
-            linha_str = str(token.linha).center(5)  # Se não, imprime o número da linha centralizado
+            # Se não, imprime o número da linha centralizado
+            linha_str = str(token.linha).center(5)
             linha_anterior = token.linha  # Atualiza a linha anterior
-            
+
         # Verifica se o tipo do token é um tipo especial que precisa de formatação diferente
         if token.tipo in tipos_especiais:
             lexema_str = token.valor.center(27)  # Se sim, centraliza o lexema
         else:
             lexema_str = " "*27  # Se não, não há lexema a ser exibido
-        
+
         # Imprime os detalhes do token formatados na tabela
-        print(f"|{linha_str}|{str(token.coluna).center(5)}|{token.tipo.center(15)}|{lexema_str}|")
+        print(f"|{linha_str}|{str(token.coluna).center(5)}|{
+              token.tipo.center(15)}|{lexema_str}|")
         print("+-----+-----+---------------+---------------------------+")
-    
+
     # Imprime uma nova linha no final
     print("\n")
+
 
 def mostrar_erros(conteudo_arquivo, lista_erros):
     # Imprime cabeçalho
     print("\n\nLista de erros:\n")
-    
+
     # Itera sobre cada linha do conteúdo do arquivo
     for num, linha in enumerate(conteudo_arquivo, start=1):
         # Imprime o número da linha seguido pelo conteúdo da linha
@@ -610,13 +634,14 @@ def mostrar_erros(conteudo_arquivo, lista_erros):
                 quant_digitos = len(str(num))
                 # Imprime a marcação de erro sob a linha com erro
                 print(f"{' ' * (quant_digitos+2)}{marcação}")
-        
+
         # Se houver erro nesta linha, imprime as mensagens de erro
         if erro_presente:
             for erro in lista_erros:
                 if erro.linha == num:
                     # Calcula a quantidade de espaços necessários para alinhar a mensagem de erro
-                    print(f"{' ' * (quant_digitos+2)}erro linha {erro.linha} coluna {erro.coluna}: {erro.msg}")
+                    print(
+                        f"{' ' * (quant_digitos+2)}erro linha {erro.linha} coluna {erro.coluna}: {erro.msg}")
 
     print()  # Imprime uma linha em branco no final
 
@@ -626,18 +651,21 @@ def printaQtdUsosToken(listaTokens):
     contagem = {}
     for token in listaTokens:
         if token.tipo in contagem:
-            contagem[token.tipo] += 1  # Incrementa o contador se o tipo de token já existe no dicionário
+            # Incrementa o contador se o tipo de token já existe no dicionário
+            contagem[token.tipo] += 1
         else:
-            contagem[token.tipo] = 1  # Inicia o contador em 1 se o tipo de token é encontrado pela primeira vez
-    
+            # Inicia o contador em 1 se o tipo de token é encontrado pela primeira vez
+            contagem[token.tipo] = 1
+
     # Ordena os tipos de token por quantidade de uso em ordem decrescente
-    contagem_ordenada = sorted(contagem.items(), key=lambda x: x[1], reverse=True)
-    
+    contagem_ordenada = sorted(
+        contagem.items(), key=lambda x: x[1], reverse=True)
+
     # Imprime cabeçalho da tabela
     print("+----------------+------+")
     print("|     TOKEN      | USOS |")
     print("+----------------+------+")
-    
+
     # Itera sobre os tipos de token ordenados por quantidade de uso
     for token, usos in contagem_ordenada:
         # Imprime o tipo de token e a quantidade de usos, centralizados na tabela
@@ -649,24 +677,25 @@ def printaQtdUsosToken(listaTokens):
 def analisador_lexico(nome_arquivo):
     # Abre o arquivo fonte especificado
     arquivo_fonte = abrir_arquivo(nome_arquivo)
-    
+
     # Lista para armazenar tokens e erros
     tokens = []
     erros = []
-    
+
     # Função que obtém tokens e erros no arquivo
-    proximo_token_e_erros(arquivo_fonte, tokens, erros) 
-    
+    proximo_token_e_erros(arquivo_fonte, tokens, erros)
+
     # Imprime a listagem de tokens reconhecidos
     print("Listagem de tokens reconhecidos:\n")
     printaTokens(tokens)
-    
+
     # Imprime a listagem das quantidades reconhecidas de cada token
     print("Listagem das quantidades reconhecidas de cada token:\n")
     printaQtdUsosToken(tokens)
-    
+
     # Mostra os erros encontrados no arquivo
     mostrar_erros(arquivo_fonte, erros)
-    
+
+
 # Chamada da a função principal com o nome do arquivo
-analisador_lexico("codigo_fonte2.cic")
+analisador_lexico("codigo_fonte3.cic")
